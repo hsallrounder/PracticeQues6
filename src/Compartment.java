fedba0f7a807db3a6d996b26018dcc04bdb85c37
Himanshu Sharma <hsallrounder@gmail.com>
2021-10-08 18:13:39 +0530

    Initial commit

---
+++ src/Compartment.java
@@ -0,0 +1,70 @@
+import java.util.Random;
+
+abstract class Compartment {
+    public abstract String notice();
+}
+class FirstClass extends Compartment{
+    @Override
+    public String notice() {
+        return "First Class";
+    }
+}
+class Ladies extends Compartment{
+    @Override
+    public String notice() {
+        return "Ladies compartment";
+    }
+}
+class General extends Compartment{
+    @Override
+    public String notice() {
+        return "General";
+    }
+}
+class Luggage extends Compartment{
+    @Override
+    public String notice() {
+        return "Luggage";
+    }
+}
+
+class TestCompartment{
+    public static void main(String[] args) {
+        Compartment[] arr = new Compartment[10];
+        Random rand= new Random();
+        int n = rand.nextInt(5);
+        System.out.println("n = "+n);
+        if(n==1){
+            arr[0] = new Luggage();
+            arr[1] = new FirstClass();
+            arr[2] = new General();
+            arr[3] = new Ladies();
+        }
+        else if(n==2){
+            arr[0] = new FirstClass();
+            arr[1] = new Ladies();
+            arr[2] = new General();
+            arr[3] = new Luggage();
+        }
+        else if(n==3){
+            arr[0] = new Ladies();
+            arr[1] = new General();
+            arr[2] = new Luggage();
+            arr[3] = new FirstClass();
+        }
+        else if(n==4){
+            arr[0] = new FirstClass();
+            arr[1] = new Luggage();
+            arr[2] = new Ladies();
+            arr[3] = new General();
+        }
+        if(n==0){
+            System.out.println("n must be from 1 to 4.");
+        }
+        else{
+            for(int i=0;i<5;i++){
+                System.out.println(arr[i].notice());
+            }
+        }
+    }
+}
